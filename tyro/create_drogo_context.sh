8bd3f9f643599744dc6834753cc65ff03f997d77
Braxton Ehle <braxton@braxtonehle.com>
2020-03-30 20:37:00 -0700

    Add drogo config entries

---
+++ tyro/create_drogo_context.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Make sure the ~/.kube/config file has the following entry in the `contexts` list:
+# - context:
+#   cluster: "kubernetes"
+#   user: "drogo"
+#   name: developer
+kubectl config set-credentials drogo --client-certificate=drogo.crt --client-key=drogo.key
