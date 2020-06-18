680fb10475893f71f9999c632210ba6f149213fe
Braxton Ehle <braxton@braxtonehle.com>
2020-06-17 19:30:54 -0700

    This turned out to not be all that useful

--- voting/setup.sh
+++ voting/setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 apt update && apt install -y nmap screen
-git clone --single-branch --branch vote https://github.com/braxtone/gameofpods.git
-kubectl taint nodes --all node-role.kubernetes.io/master-
+# Turns out this was just a one-off in the lab where it came up without a running node
+# kubectl taint nodes --all node-role.kubernetes.io/master-
