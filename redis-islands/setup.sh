b1f69ed94b962f4c201392dd0b3dad1cfdc3e8a1
Braxton Ehle <braxton@braxtonehle.com>
2020-06-27 15:54:52 -0700

    Install python3.7 for sane string interpolation, F STRINGS 4 LYFE

--- redis-islands/setup.sh
+++ redis-islands/setup.sh
@@ -1,2 +1,6 @@
 for i in $(seq 5 $END); do ssh node01 mkdir "/redis0$i"; done
-python3 ./redis-pv.py
+apt update
+sudo apt install -y software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt install -y python3.7
+python3.7 ./redis-pv.py
