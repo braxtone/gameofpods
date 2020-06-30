f124b6cd3b1333f97a2b847178eefae18bacde7c
Braxton Ehle <braxton@braxtonehle.com>
2020-06-29 21:15:08 -0700

    Remove SSH key checking, fix python3.7 install

--- redis-islands/setup.sh
+++ redis-islands/setup.sh
@@ -1,6 +1,8 @@
-for i in $(seq 6 $END); do ssh node01 mkdir "/redis0$i"; done
+for i in $(seq 6 $END); do ssh -o "StrictHostKeyChecking=no" node01 mkdir "/redis0$i"; done
 apt update
+# This is worth it just to get proper string interpolation right? RIGHT?!?1
 sudo apt install -y software-properties-common
 sudo add-apt-repository ppa:deadsnakes/ppa
+apt update
 sudo apt install -y python3.7
 python3.7 ./redis-pv.py
